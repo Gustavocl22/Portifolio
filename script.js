a87544e8efa47e17a8a44646b88b2266b31132bf
Gustavo Catucci <gustavocatucci@gmail.com>
2025-02-04 17:56:42 -0300

    Atualizacao do projeto

--- script.js
+++ script.js
@@ -1,28 +1,49 @@
 const menuIcon = document.getElementById("menu-icon");
 const navbar = document.getElementById("navbar");
 const navLinks = document.querySelectorAll(".navbar a");
+const sections = document.querySelectorAll("section");
+
 
 menuIcon.addEventListener("click", () => {
-    navbar.style.display = navbar.style.display === "flex" ? "none" : "flex";
+    navbar.classList.toggle("active");
 });
 
 
 navLinks.forEach(link => {
     link.addEventListener("click", () => {
         if (window.innerWidth < 769) {
-            navbar.style.display = "none";
+            navbar.classList.remove("active");
         }
-        
-        navLinks.forEach(item => item.classList.remove("active"));
-        link.classList.add("active");
+    });
+});
+
+
+window.addEventListener("scroll", () => {
+    let currentSection = "";
+
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop - 100;
+        const sectionHeight = section.clientHeight;
+
+        if (window.scrollY >= sectionTop && window.scrollY < sectionTop + sectionHeight) {
+            currentSection = section.getAttribute("id");
+        }
+    });
+
+    navLinks.forEach(link => {
+        link.classList.remove("active");
+        if (link.getAttribute("href").includes(currentSection)) {
+            link.classList.add("active");
+        }
     });
 });
 
 
 window.addEventListener("resize", () => {
     if (window.innerWidth >= 769) {
-        navbar.style.display = "flex"; 
+        navbar.classList.add("desktop"); 
+        navbar.classList.remove("active"); 
     } else {
-        navbar.style.display = "none"; 
+        navbar.classList.remove("desktop"); 
     }
 });
